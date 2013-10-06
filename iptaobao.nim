77eb771d66185408241eb99b1fc4d7d66778af5b
itang <live.tang@gmail.com>
2013-10-06 13:21:55 +0800

    first commit

---
+++ iptaobao.nim
@@ -0,0 +1,57 @@
+discard """
+  iptaobao:
+  Nimrod wrapper for ip.taobao.com"
+"""
+
+import json, httpclient #, sockets
+
+type
+  TIpInfo* = tuple
+    countryId: string
+    country:   string
+    area:      string
+    areaId:    string
+    region:    string
+    regionId:  string
+    city:      string
+    cityId:    string
+    isp:       string
+    ispId:     string
+    ip:        string
+
+  TRet* = tuple[ipInfo: TIpInfo, err: string]
+
+const RestApiUrlPrefix* = "http://ip.taobao.com/service/getIpInfo.php?ip="
+
+proc getIpInfo*(ip: string, timeout = 5 * 1000): TRet =
+  try:
+    let content = httpclient.getContent(url = RestApiUrlPrefix & ip, timeout = timeout)
+    let jcontent = json.parseJson(content)
+    let data = jcontent["data"]
+    if jcontent["code"].num == 0:
+      result.ipInfo = (countryId: data["country_id"].str,
+                       country:   data["country"].str,
+                       area:      data["area"].str,
+                       areaId:    data["area_id"].str,
+                       region:    data["region"].str,
+                       regionId:  data["region_id"].str,
+                       city:      data["city"].str,
+                       cityId:    data["city_id"].str,
+                       isp:       data["isp"].str,
+                       ispId:     data["isp_id"].str,
+                       ip:        data["ip"].str)
+    else:
+      result.err = data.str
+  except:
+    let
+      e = getCurrentException()
+      msg = getCurrentExceptionMsg()
+    result.err = "Got exception " & repr(e) & " with message " & msg
+
+when isMainModule:
+  for ip in ["888.888.888.888", "8.8.8.8"]:
+    let (ipInfo, err) = getIpInfo(ip)
+    if err != nil:
+      echo "error: " & err
+    else:
+      echo "ok: " & $ipInfo
---
+++ test.nim
@@ -0,0 +1,4 @@
+import iptaobao
+
+let ret = getIpInfo("8.8.8.8")
+echo "country: ", ret.ipInfo.country
